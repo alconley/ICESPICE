b5b9d19b5a086a9531f2e01ac14f4b2b01f7a54a
alconley <imaconley1@gmail.com>
2024-04-26 09:58:42 -0400

    modified:   ICESPICE.mac 	modified:   python_scripts/MacroCreation.py 	modified:   src/ICESPICEDetectorConstruction.cc 	modified:   src/ICESPICEEventAction.cc 	modified:   src/ICESPICEPrimaryGeneratorAction.cc 	modified:   src/ICESPICERunAction.cc 	modified:   src/ICESPICESteppingAction.cc

--- python_scripts/MacroCreation.py
+++ python_scripts/MacroCreation.py
@@ -9,25 +9,27 @@ with open(macro_path, 'w') as file:
     file.write('/control/verbose 1\n')
     file.write('/run/verbose 0\n')
     file.write('/event/verbose 0\n')
-    file.write('/run/initialize\n')
     
     # Loop through the different thicknesses of our PIPS detectors
-    for thickness in [100, 300, 500, 1000]:
+    # for thickness in [100, 300, 500, 1000]:
+    for thickness in [1000]:
         # Write the command to set the thickness
         file.write(f'/ICESPICE/Detector/Thickness {thickness}\n')
         
         # Loop through the positions from -20 to -60 in steps of 5
-        for position in range(-20, -65, -5):
+        for position in range(-20, -25, -5):
             # Write the command to set the position
             file.write(f'/ICESPICE/Detector/Position {position}\n')
             
             # Loop through energies from 50 keV to 2000 keV in steps of 50 keV
-            for energy in range(50, 2050, 50):
+            for energy in range(100, 2000, 100):
                 # Write the command to set the energy
                 file.write(f'/gun/energy {energy} keV\n')
                 
                 # Write the command to set the filename of the output root file
                 file.write(f'/analysis/setFileName ICESPICE_PIPS{thickness}_f50mm_g{abs(position)}mm_{energy}keV.root\n')
+                
+                file.write('/run/initialize\n')
                 
                 # Write the command to run the simulation
                 file.write(f'/run/beamOn {n_particles}\n')
